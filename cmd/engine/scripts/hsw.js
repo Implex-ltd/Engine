e7b340791f499d8ab1aa521928195d9a1d5e19b9
0xF7A4C6 <0xF7A4C6@proton.me>
2023-08-21 18:39:27 +0200

    Update hsw.js

--- cmd/engine/scripts/hsw.js
+++ cmd/engine/scripts/hsw.js
@@ -4085,7 +4085,7 @@ var hsw = function () {
         bg[g] = A
 
         switch (g) {
-            case 48:
+            case A instanceof Window:
                 A.Navigator.appVersion = "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
                 A.Navigator.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
                 A.Navigator.deviceMemory = 8
