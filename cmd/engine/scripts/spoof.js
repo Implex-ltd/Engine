5e1e964b6d58a15562245e25307a616a8e70d3e0
0xF7A4C6 <0xF7A4C6@proton.me>
2023-09-09 23:03:51 +0200

    spoofs

--- cmd/engine/scripts/spoof.js
+++ cmd/engine/scripts/spoof.js
@@ -9,7 +9,24 @@ function addNoiseToCanvas() {
         originalFillText.call(this, text, x, y, maxWidth);
     };
 }
-    
+
+Object.defineProperty(performance, "now", {
+    value: function () {
+        return 14112.800000000745 + (Math.random() * 7000.000000000000);
+    }
+});
+
+Object.defineProperty(SVGRect.prototype, "x", { value: Math.floor(Math.random() * 100) + 111 })
+Object.defineProperty(SVGRect.prototype, "y", { value: Math.floor(Math.random() * 100) + 111 })
+
+Object.defineProperty(SVGRect.prototype, "height", { value: Math.floor(Math.random() * 100) + 111 })
+Object.defineProperty(SVGRect.prototype, "width", { value: Math.floor(Math.random() * 100) + 111 })
+
+Object.defineProperty(SVGRectElement.prototype, "getBBox", { value: Math.floor(Math.random() * 100) + 111 })
+
+Object.defineProperty(SVGTextContentElement.prototype, "getSubStringLength", { value: () => Math.floor(Math.random() * 100) + 111 })
+Object.defineProperty(SVGTextContentElement.prototype, "getComputedTextLength", { value: () => Math.floor(Math.random() * 100) + 111 })
+
 function spoofall() {
     addNoiseToCanvas();
 }
